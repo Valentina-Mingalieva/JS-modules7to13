8691a5570b0e3e1ea3b9d887bf5dfee05eeedd9e
Valentina Mingalieva <valentina.mingalieva@gmail.com>
2021-05-25 20:35:42 +0200

    m

--- js/seminar8-1.js
+++ js/seminar8-1.js
@@ -45,6 +45,79 @@ function handleMazeLeave(e) {
 
 // 2. Paint block when mouse on button
 
+const mouseOverListener = e => {
+  if (e.target.classList.contains('card__delete')) {
+    e.currentTarget.classList.add('card_focus');
+  }
+};
+
+const mouseOutListener = e => {
+  if (e.target.classList.contains('card__delete')) {
+    e.currentTarget.classList.remove('card_focus');
+  }
+};
+
+const cardRef = document.querySelector('.card');
+
+cardRef.addEventListener('mouseover', mouseOverListener);
+cardRef.addEventListener('mouseout', mouseOutListener);
+
 // 3. Create accordion
 
-// 4. Create debounce and throttle. Write in console scroll time
+const treeRef = document.querySelector('.tree');
+
+treeRef.addEventListener('click', event => {
+  if (!event.target.classList.contains('tree__item')) return;
+
+  const subTreeRef = event.target.querySelector('.tree__sub-tree');
+  if (!subTreeRef) return;
+
+  if (subTreeRef.classList.contains('tree__sub-tree_open')) {
+    subTreeRef.querySelectorAll('.tree__sub-tree').forEach(ref => {
+      ref.classList.remove('tree__sub-tree_open');
+    });
+  }
+
+  subTreeRef.classList.toggle('tree__sub-tree_open');
+});
+
+// 4. Create debounce and throttle. Write in console scroll time.
+
+const divEl = document.querySelector("#my-id");
+
+function handleBodyScroll() {
+    console.log("scroll", new Date());
+}
+
+function throttle(callback, delay) {
+    let id;
+
+    return function () {
+        if (id) {
+            return;
+        }
+
+        id = setTimeout(() => {
+            callback();
+            id = 0;
+        }, delay);
+    }
+}
+
+// divEl.addEventListener("scroll", throttle(handleBodyScroll, 1000));
+
+function debounce(callback, delay) {
+    let id;
+
+    return function () {
+        if (id) {
+            clearTimeout(id);
+        }
+        id = setTimeout(() => {
+            callback();
+            id = 0;
+        }, delay);
+    }
+}
+
+divEl.addEventListener("scroll", debounce(handleBodyScroll, 1000));
